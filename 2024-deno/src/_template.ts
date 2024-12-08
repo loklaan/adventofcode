561a64ffbf50d45096aca13e8fa34a434b70ee6f
Lochlan Bunn <bunn@lochlan.io>
2024-12-08 19:37:57 +1100

    fix 2024 template .ts

--- 2024-deno/src/_template.ts
+++ 2024-deno/src/_template.ts
@@ -3,7 +3,7 @@ import { Solution } from "@/lib/cli.ts";
 export const solution: Solution = async ({ loadInput, debug, answer }) => {
   debug(`Start.`);
   const input = await loadInput();
-  const lines = input.split("\n").map((l) => l.trim()).filter((x) => x);
+  const lines = input.trim().split("\n");
 
   debug("Done.");
   answer("");
